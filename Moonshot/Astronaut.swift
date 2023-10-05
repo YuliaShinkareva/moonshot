f3428443ec215192d0afb7944190b9be9fd34baa
Julia <js.shinkareva@gmail.com>
2023-10-05 14:14:18 +0200

    Committing all changes before pushing

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astranaut.swift
+//  Moonshot
+//
+//  Created by yulias on 14/05/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
